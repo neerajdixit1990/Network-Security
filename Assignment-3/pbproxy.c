7973a5b46509c92ea279a8283e14fb089d517c9b
Neeraj Dixit <neerajdixit1990@gmail.com>
2016-04-02 03:09:34 -0400

    added basic code for TCP connections

--- Assignment-3/pbproxy.c
+++ Assignment-3/pbproxy.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/aes.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <ctype.h>
 
 /* AES key for Encryption and Decryption */
 const static unsigned char aes_key[]={0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF};
@@ -53,20 +58,186 @@ void print_data(const char *tittle, const void* data, int len)
 	printf("\n");
 }
 
+int	
+client_connections(char		*destn,
+		   int		destn_port,
+		   int		*ret) {
+	
+	int 			soc_fd, status; 
+	struct sockaddr_in 	client;
+
+	soc_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (soc_fd < 0) {
+		printf("\nUnable to create socket for client !!!\n");
+		return -1;
+	}
+
+    	bzero(&client, sizeof(client));
+    	client.sin_family = AF_INET;
+    	client.sin_port = htons(destn_port);
+    	inet_pton(AF_INET, destn, &client.sin_addr);
+	
+	/* Socket connect, no need to bind */
+	status = connect(soc_fd, (struct sockaddr *)&client, sizeof(client));
+	if (status != 0) {
+		printf("\nUnable to connect client on requested connection !!!\n");
+		return -1;
+	}
+	
+	*ret = soc_fd;	
+	return 0;	
+}
+
+int
+client_launch(int	soc_fd) {
+	int	status = -1;
+
+	while(1) {
+		fd_set 		mon_fd;
+		
+		FD_ZERO(&mon_fd);
+		FD_SET(soc_fd, &mon_fd);
+		FD_SET(fileno(stdin), &mon_fd);
+		
+		/* Monitor keyboard input and socket input */
+		status = select(soc_fd + 1, &mon_fd, NULL, NULL, NULL);
+		if (status < 0) {
+			printf("\nStatus = %d, Unable to monitor sockets !!! Exiting ...\n",status);
+			return 0;
+		}
+		
+		if (FD_ISSET(fileno(stdin), &mon_fd)) {
+			printf("\nSocket has data :)");
+		} else if (FD_ISSET(soc_fd, &mon_fd)) {
+			printf("\nstdin has data :)");
+		} else {
+			printf("\nAbnormal return by select() function ...");
+		}
+	}
+	return 0;	
+}
+
+int
+server_connections(char		*destn,
+		   int		destn_port,
+		   int		proxy_port,
+		   int		*destn_ret,
+		   int		*proxy_ret) {
+        int                     destn_soc_fd, status, proxy_soc_fd;
+        struct sockaddr_in      server;
+
+        destn_soc_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (destn_soc_fd < 0) {
+                printf("\nUnable to create socket for server !!!\n");
+                return -1;
+        }
+
+    	bzero(&server, sizeof(server));
+    	server.sin_family = AF_INET;
+	inet_pton(AF_INET, destn, &server.sin_addr);
+	server.sin_port = htons (destn_port);
+
+    	status = bind(destn_soc_fd, (struct sockaddr *)&server, sizeof(server));
+	if (status != 0) {
+		printf("\nStatus = %d, Unable to bind server socket !!!",status);
+		return -1;
+	}
+
+	status = listen(destn_soc_fd, 20);
+	if (status != 0) {
+		printf("\nStatus = %d, Unable to listen on server socket !!!",status);
+		return -1;
+	}
+	*destn_ret = destn_soc_fd;
+
+
+        proxy_soc_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (proxy_soc_fd < 0) {
+                printf("\nUnable to create socket for server !!!\n");
+                return -1;
+        }
+
+        bzero(&server, sizeof(server));
+        server.sin_family = AF_INET;
+	inet_pton(AF_INET, destn, &server.sin_addr);	
+        server.sin_port = htons(proxy_port);
+
+        status = bind(proxy_soc_fd, (struct sockaddr *)&server, sizeof(server));
+        if (status != 0) {
+                printf("\nStatus = %d, Unable to bind server socket !!!",status);
+                return -1;
+        }
+
+        status = listen(proxy_soc_fd, 20);
+        if (status != 0) {
+                printf("\nStatus = %d, Unable to listen on server socket !!!",status);
+                return -1;
+        }
+        *proxy_ret = proxy_soc_fd;
+
+	return 0;
+}
+
+int
+server_launch(int	destn_soc_fd,
+	      int	proxy_soc_fd) {
+        int     status = -1;
+
+        while(1) {
+                fd_set          mon_fd;
+
+                FD_ZERO(&mon_fd);
+                FD_SET(destn_soc_fd, &mon_fd);
+                FD_SET(proxy_soc_fd, &mon_fd);
+
+                if (destn_soc_fd > proxy_soc_fd)
+			status = select(destn_soc_fd + 1, &mon_fd, NULL, NULL, NULL);
+		else
+			status = select(proxy_soc_fd + 1, &mon_fd, NULL, NULL, NULL);
+                if (status < 0) {
+                        printf("\nStatus = %d, Unable to monitor sockets on server !!! Exiting ...\n",status);
+                        return 0;
+                }
+
+                if (FD_ISSET(destn_soc_fd, &mon_fd)) {
+                        printf("\ndestination Socket has data :)");
+                } else if (FD_ISSET(proxy_soc_fd, &mon_fd)) {
+                        printf("\nproxy socket has data :)");
+                } else {
+                        printf("\nAbnormal return by select() function ...");
+                }
+        }
+        return 0;
+
+}
+
 int main(int argc, char **argv) {
-        while ((option = getopt(argc, argv, "i:r:s:")) != -1) {
+        int     	count = 0, soc_fd = -1;
+	int		destn_soc_fd = -1, proxy_soc_fd = -1;
+        char    	option;
+        char    	*key_file = NULL;
+        char    	destn[50] = {0}, *ptr = NULL;
+        int     	index = -1, status;
+	int		destn_port = -1, proxy_port = -1;
+	long long int	temp = -1;
+
+	if (argc < 4) {
+		printf("\nPlease enter correct number of arguments !!!\n");
+		return 0;
+	}
+	
+        while ((option = getopt(argc, argv, "l:k:")) != -1) {
                 switch(option) {
-                        case 'i':
-                                interface = optarg;
-                                printf("\nSniffing on interface: %s\n", interface);
+                        case 'k':
+                                key_file = optarg;
                         break;
-                        case 'r':
-                                filename = optarg;
-                                printf("\nReading from pcap file: %s\n", filename);
-                        break;
-                        case 's':
-                                payload_string = optarg;
-                                printf("\nPayload filter applied is: %s\n", payload_string);
+                        case 'l':
+				temp = strtol(optarg, &ptr, 10);
+				if (temp < 0 || temp > 65535) {
+					printf("\nDestination Port numbers must be between 0-65535\n");
+					return 0;
+				}
+				proxy_port = (int)temp;
                         break;
                         case '?':
                                 printf("\nUn-supported option passed !!!\n");
@@ -78,16 +249,43 @@ int main(int argc, char **argv) {
                 count++;
         }
 
-        if (2*count+1 < argc) {
-                index = 2*count + 1;
-                while(1) {
-                        if (argv[index] == NULL || strstr((const char *)argv[index], "-"))
-                                break;
-                        strcat(filter_string, (const char *)argv[index]);
-                        strcat(filter_string, " ");
-                        index++;
-                }
-                printf("\nSetting BPF filter: %s\n", filter_string);
-        }
-	
+        index = 2*count + 1;
+	strncpy(destn, argv[index], strlen(argv[index]));
+        temp = strtol(argv[++index], &ptr, 10);
+        if (temp < 0 || temp > 65535) {
+        	printf("\nProxy Port numbers must be between 0-65535\n");
+        	return 0;
+        } 
+	destn_port = (int)temp;
+	printf("\nKey File = %s, proxy port = %d, destn = %s, destn Port = %d\n", key_file, proxy_port, destn, destn_port);
+
+	if (proxy_port == -1) {
+		/* client mode */
+		status = client_connections(destn, destn_port, &soc_fd);
+		if (status != 0) {
+			printf("\nUnable to establish client connections !!!\n");
+			return -1;
+		}
+
+		status = client_launch(soc_fd);
+		if (status != 0) {
+			printf("\nClient unable to communicate with server !!!\n");
+			return -1;
+		}
+	} else {
+		/* server mode */
+		status = server_connections(destn, destn_port, proxy_port, &destn_soc_fd, &proxy_soc_fd);
+                if (status != 0) {
+                        printf("\nUnable to establish client connections !!!\n");
+                        return -1; 
+                }   
+
+                status = server_launch(destn_soc_fd, proxy_soc_fd);
+                if (status != 0) {
+                        printf("\nClient unable to communicate with server !!!\n");
+                        return -1; 
+                }   
+	}
+
+	return 0;
 }
